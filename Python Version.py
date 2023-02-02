fde25850c401cef3d3e9b0c7bb4c8ef7391f0feb
Mikaizlit <124289959+Mikaizlit@users.noreply.github.com>
2023-02-02 10:43:07 -0700

    Add files via upload

---
+++ Python Version.py
@@ -0,0 +1,10 @@
+name = input("What is your name?: ")
+age = int(input("how old are you?: "))
+height = float(input("How tall are you?: "))
+
+
+print("You are "+str(age)+"CM tall!")
+print("Hello "+name)
+print("You are "+str(age)+" years old")
+
+input("did you like this??????")
